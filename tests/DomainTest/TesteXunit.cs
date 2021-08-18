3312faa9370ab462a075eca9df5b7534eecb1174
Lucas <lucasgunner3@gmail.com>
2021-08-18 16:50:43 -0300

    teste do teste

---
+++ tests/DomainTest/TesteXunit.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace DomainTest
+{
+    public class TesteXunit
+    {
+        [Fact(DisplayName = "IgualdadeVariaveis")]
+        public void VariaveisDevemTerMesmoValor()
+        {
+            //AAA
+            //Organizaçãp
+            var number1 = 2;
+            var number2 = 1;
+
+            //Ação
+            number1 = number2;
+
+            //Assert
+            Assert.Equal(number1, number2);
+        }
+    }
+}
